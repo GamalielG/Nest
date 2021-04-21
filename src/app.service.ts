64edf4b451bb3bfc3f71fb0343b97cc8a88bd8f7
Gamaliel <gamaliel.gonzalez.is@unipolidgo.edu.mx>
2021-04-21 16:34:15 -0500

    cambios

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 
-@Injectable() //Injectable indica la inyeccion de dependencias
+@Injectable() 
 export class AppService {
   getHello(): string {
     return 'Hello World!';
